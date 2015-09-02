dcb8e38f5fa1ce7abf29468014b373a4d34a74ea
Cerek Hillen <crockeo@gmail.com>
2015-09-02 13:37:23 -0400

    Starting on some synth stuff.

---
+++ synth/main.go
@@ -0,0 +1,11 @@
+package synth
+
+// Starting the synth with static synth data.
+func StartStaticSynth() {
+
+}
+
+// Starting the synth with a channel for note data.
+func StartDynamicSynth() {
+
+}
