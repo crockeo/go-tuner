b2a5960ac01431439996ba0546e954329916c0b3
Cerek Hillen <crockeo@gmail.com>
2015-09-05 00:29:33 -0400

    Moved some stuff around and hopefully got file loading working.

--- filestore/main.go
+++ filestore/main.go
@@ -1,11 +1,28 @@
 package filestore
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/crockeo/go-tuner/synth"
+	"os"
 )
 
 // Trying to load a synth.NoteArrangement from a file on the disk.
 func LoadNoteArrangement(path string) (*synth.NoteArrangement, error) {
-	return nil, errors.New("not yet implemented")
+	if _, err := os.Stat(path); err != nil {
+		return nil, errors.New("File \"" + path + "\" does not exist.")
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	na := new(synth.NoteArrangement)
+	err = json.NewDecoder(file).Decode(na)
+	if err != nil {
+		return nil, err
+	}
+
+	return na, nil
 }
--- server/handler.go
+++ server/handler.go
@@ -1,8 +1,22 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/crockeo/go-tuner/synth"
 )
+
+//// Attempting to parse a given message into a DelayedNote. It will return
+//// an error upon failure.
+func ParseMessage(str string) (synth.DelayedNoteData, error) {
+	var dnd synth.DelayedNoteData
+	err := json.NewDecoder(bytes.NewBufferString(str)).Decode(&dnd)
+	if err != nil {
+		return synth.DelayedNoteData{}, nil
+	}
+
+	return dnd, nil
+}
 
 // Attempting to handle a message and send the parsed data over to the synth
 // through a channel.
@@ -13,7 +27,7 @@ func HandleMessage(str string, noteChannel chan synth.DelayedNoteData) error {
 		return nil
 	}
 
-	dnd, err := synth.ParseMessage(str)
+	dnd, err := ParseMessage(str)
 	if err != nil {
 		return err
 	}
